4dc01171bc4451a2468fb0d9a11cca4a8358a583
OskariV <34027142+OskariV@users.noreply.github.com>
2018-03-11 20:05:45 +0200

    kommentoitu

--- Selfdestruct.cs
+++ Selfdestruct.cs
@@ -6,13 +6,17 @@ namespace Units
 {
 	public class Selfdestruct : MonoBehaviour
 	{
-
+		// Used by laser object to find collision with other objects.
 		void OnTriggerEnter2D (Collider2D other)
 		{
+			// If interacts with anything other than a radar collider.
 			if (!other.tag.Contains ("Radar")) {
+				// Uses Unitys SendMessage function to use other colliders Takedamage method to deal damage.
 				other.SendMessage ("TakeDamage", 5f);
+				// Creates an explosion sprite.
 				GameObject explosion = Instantiate (Resources.Load ("Prefabs/LaserImpact", typeof(GameObject))) as GameObject;
 				explosion.transform.position = transform.position;
+				// Destroys the explosion sprite and itself.
 				Destroy (explosion, 0.5f);
 				Destroy (this.gameObject);
 			}

8c4f1b6f7068fa8ed2ddc9a1a03e551d7c47d347
OskariV <34027142+OskariV@users.noreply.github.com>
2018-03-11 20:00:06 +0200

    kommentoitu

--- Laser.cs
+++ Laser.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
-
+	// Sets firerate
 	private float fireRateLaser = 5f;
 	private float fireSpeed = 1000f;
 	private float fireTime = 0;
 
 	public void Shoot ()
 	{
+		// If enough time has passed creates laser object and destroys it if it travels too long without colliding with an object.
 		if (Time.time > fireTime) {
 			fireTime = Time.time + 1 / fireRateLaser;
 			GameObject bullet = Instantiate (Resources.Load ("Prefabs/blastLaser 1", typeof(GameObject))) as GameObject;

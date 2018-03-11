7c068dd0ea11c674754b54f6476d227f5090b696
OskariV <34027142+OskariV@users.noreply.github.com>
2018-03-11 19:42:58 +0200

    kommentoitu

--- JunkerAI.cs
+++ JunkerAI.cs
@@ -15,7 +15,7 @@ namespace Units
 
 		public float enemyHitPoints { get; set; }
 
-
+		//Sets health, speed, finds it's own radars and weapons and targets player.
 		void Start ()
 		{
 			this.playerShip = GameObject.Find ("PlayerShip");
@@ -30,19 +30,21 @@ namespace Units
 		{
 			if (this.playerShip != null) {
 				this.RepairShip ();
+				//aims towards player.
 				Vector3 targetPos = Camera.main.WorldToScreenPoint (playerShip.transform.position);
 				Vector3 selfPos = Camera.main.WorldToScreenPoint (this.transform.position);
 				selfPos.x = selfPos.x - targetPos.x;
 				selfPos.y = selfPos.y - targetPos.y;
 				float angle = Mathf.Atan2 (selfPos.y, selfPos.x) * Mathf.Rad2Deg;
 				bool targetInRange = radar.RadarPing ();
-
+				//Moves towards player when radar not in range.
 				if (targetInRange == false) {
 					
 					this.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle - 270));
 					this.transform.Translate (0, 0.01f * enemyShipSpeed, 0, Space.Self);
 				
 				}
+				// Circles player when radar in range and fires missiles.
 				if (targetInRange == true) {
 					this.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle - 180));
 					this.transform.Translate (0, 0.01f * enemyShipSpeed, 0, Space.Self);
@@ -52,6 +54,7 @@ namespace Units
 			}
 
 		}
+		// Makes ship take damage and destroys it when health is depleted.
 		public void TakeDamage (float x)
 		{
 			enemyHitPoints = enemyHitPoints - x;
@@ -60,6 +63,7 @@ namespace Units
 				Destroy (this.gameObject);
 			}
 		}
+		// Repairs ship if health is below full.
 		private void RepairShip ()
 		{
 			if (this.enemyHitPoints < 100f) {

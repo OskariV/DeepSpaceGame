a01012250ad39c81ea1700c46f76019e5837a9d3
OskariV <34027142+OskariV@users.noreply.github.com>
2018-03-11 19:52:34 +0200

    kommentoitu

--- CarrierSubSpawner.cs
+++ CarrierSubSpawner.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Sets spawn rate.
 public class CarrierSubSpawner : MonoBehaviour {
 	private float spawnTime = 0;
 	private float spawnRate = 0.2f;
 	public void Spawn ()
 	{
+		// If enough time passed spawns Ufo unit.
 		if (Time.time > spawnTime) {
 			spawnTime = Time.time + 1 / spawnRate;
 

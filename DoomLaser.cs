320b67615b502a41d850a5d0497305850c3dd615
OskariV <34027142+OskariV@users.noreply.github.com>
2018-03-11 19:58:59 +0200

    kommentoitu

--- DoomLaser.cs
+++ DoomLaser.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class DoomLaser : MonoBehaviour 
 	{
-
+		// Sets firerate.
 		private float fireRateLaser = 0.5f;
 		private float fireSpeed = 1000f;
 		private float fireTime = 0;
 
 		public void Shoot ()
 		{
+			// If enough time has passed creates doomlaser object and destroys it if it travels too long without colliding with an object.
 			if (Time.time > fireTime) {
 				fireTime = Time.time + 1 / fireRateLaser;
 				GameObject bullet = Instantiate (Resources.Load ("Prefabs/IsoLaser", typeof(GameObject))) as GameObject;

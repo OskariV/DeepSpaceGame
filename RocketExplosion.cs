1f97e13b7036f15127a3d79a759d58d3a74d470f
OskariV <34027142+OskariV@users.noreply.github.com>
2018-03-11 19:24:50 +0200

    kommentoitu

--- RocketExplosion.cs
+++ RocketExplosion.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class RocketExplosion : MonoBehaviour
 {
-
+	// Used by missile object to find collision with other objects.
 	void OnTriggerEnter2D (Collider2D other)
 	{
+		// If interacts with anything other than a radar collider.
 		if (!other.tag.Contains ("Radar")) {
+			// Uses Unitys SendMessage function to use other colliders Takedamage method to deal damage.
 			other.SendMessage ("TakeDamage", 50f);
+			// Creates an explosion sprite.
 			GameObject explosion = Instantiate (Resources.Load ("Prefabs/Explosion", typeof(GameObject))) as GameObject;
 			explosion.transform.position = transform.position;
+			// Destroys the explosion sprite and itself.
 			Destroy (explosion, 0.5f);
 			Destroy (this.gameObject);
 		}

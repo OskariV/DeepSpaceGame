a3466985425a4819f0dd65aa48e352a52496775c
OskariV <34027142+OskariV@users.noreply.github.com>
2018-03-11 18:09:00 +0200

    Update Weapon.cs

--- Weapon.cs
+++ Weapon.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-
+	//Sets the fire rate of weapons and the speed of projectiles.
 	private float fireRateLaser = 5f;
 	private float fireRateRocket = 1f;
 	private float fireSpeed = 1000f;
@@ -15,10 +15,11 @@ public class Weapon : MonoBehaviour
 	void Update ()
 	{
 
-
+		
 		if (fireRateLaser == 0 || fireRateRocket == 0) {
 			Debug.LogError ("Cant Shoot Fire rate 0");
 		} else {
+			//fires if enough time has passed since last firing.
 			if (Input.GetMouseButton (0) && Time.time > fireTime) {
 				fireTime = Time.time + 1 / fireRateLaser;
 				Shoot ();
@@ -32,7 +33,7 @@ public class Weapon : MonoBehaviour
 
 	void Shoot ()
 	{
-
+		// creates a laser object and gives it a direction and speed.
 		GameObject bullet = Instantiate (Resources.Load ("Prefabs/blastLaser 1", typeof(GameObject))) as GameObject;
 		bullet.transform.position = transform.position + transform.up * 1f;
 		bullet.transform.rotation = transform.rotation;
@@ -44,6 +45,7 @@ public class Weapon : MonoBehaviour
 
 	void rocket ()
 	{
+		// creates a missile object and gives it a direction and speed.
 		GameObject rocket = Instantiate (Resources.Load ("Prefabs/rocketPic 1", typeof(GameObject))) as GameObject;
 		rocket.transform.position = transform.position + transform.up * 1f;
 		rocket.transform.rotation = transform.rotation;
